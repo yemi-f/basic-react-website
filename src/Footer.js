628d63f18ed4f5deb640ee9bc861fccb5e4f7bc0
Yemi Fakorede <yemifakorede@gmail.com>
2020-07-22 19:39:59 -0400

    changed footer text

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="my-2 p-2 text-center">
-            © 2020 Adeyemi Fakorede
+            © 2020 Footer Inc.
         </footer>
     )
 }
