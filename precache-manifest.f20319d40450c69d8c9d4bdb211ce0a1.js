d5e660ca2441482cd2da06d7d97f8f4bdd500761
Yemi Fakorede <yemifakorede@gmail.com>
2020-07-22 16:52:03 -0400

    Updates

---
+++ precache-manifest.f20319d40450c69d8c9d4bdb211ce0a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9da433bb43a3438eafdc816a03b39d",
+    "url": "/basic-react-website/index.html"
+  },
+  {
+    "revision": "bedfe6d9df8cdf525a6c",
+    "url": "/basic-react-website/static/css/main.d68390b2.chunk.css"
+  },
+  {
+    "revision": "38aa9bfedb21159f7ba6",
+    "url": "/basic-react-website/static/js/2.16da7c14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-react-website/static/js/2.16da7c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedfe6d9df8cdf525a6c",
+    "url": "/basic-react-website/static/js/main.95596c9e.chunk.js"
+  },
+  {
+    "revision": "8df2494f64e1e54f1dc6",
+    "url": "/basic-react-website/static/js/runtime-main.9e3d251e.js"
+  },
+  {
+    "revision": "1381722e7c790d0361a18597fde94945",
+    "url": "/basic-react-website/static/media/Heart.1381722e.png"
+  },
+  {
+    "revision": "ca469ad8deaa45cab2c7ae256b609cac",
+    "url": "/basic-react-website/static/media/Smile.ca469ad8.png"
+  },
+  {
+    "revision": "db845774766c140e2169640780e0524e",
+    "url": "/basic-react-website/static/media/Tablet.db845774.png"
+  }
+]);
